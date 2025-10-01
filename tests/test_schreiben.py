909d9d5ce2b1583eeffda2334e51c2ac7702b69e
LStrunk-stud <lucastrunk@stud.th-owl.de;git config --global user.email lucastrunk@stud.th-owl.de>
2025-10-01 15:39:48 +0200

    Meine Änderungen an LODEGP.py hinzugefügt

--- tests/test_schreiben.py
+++ tests/test_schreiben.py
@@ -13,10 +13,11 @@ from lodegp.LODEGP import LODEGP
 import numpy as np
 from GP_helpers.helpers.util_functions import calculate_differential_equation_error_symbolic, calculate_differential_equation_error_numeric
 import pytest
+import matplotlib.pyplot as plt
 
 torch.set_default_dtype(torch.float64)
 
-def symmetry_check(model):
+def symmetry_check(model): 
     dx1, dx2 = var('dx1, dx2')
     symmetry_check_matrix = model.matrix_multiplication - matrix([[cell.substitute(dx1=dx2, dx2=dx1) for cell in row] for row in model.matrix_multiplication.T])
     # Symmetric up to numerical precision
@@ -77,51 +78,96 @@ def test_bipendulumkernel(base_kernel):
     model.sage_locals = {'x': var('x')}
     
 @pytest.mark.parametrize("base_kernel", ["SE_kernel", "Matern_kernel_32", "Matern_kernel_52"])
-def test_threetank(base_kernel):
+def test_three_tank(base_kernel):
     train_x = torch.linspace(0, 1, 10)
     train_y = torch.linspace(0, 1, 10)
-    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(5, noise_constraint=gpytorch.constraints.Positive())
-    model = LODEGP(train_x, train_y, likelihood, 5, ODE_type="Three tank", base_kernel=base_kernel)
+    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(5)
+    model = LODEGP(train_x, train_y, likelihood, 5, ODE_name="Three tank", base_kernel=base_kernel)
     eig_positive, eig_real = eigval_check(model, train_x)
+    
     assert eig_positive
     assert eig_real
-    model.sage_locals = {'x': var('x')}
 
 def test_calculate_differential_equation_error_symbolic():
     train_x = torch.linspace(0, 1, 10)
     train_y = torch.linspace(0, 1, 10)
     likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(3)
     model = LODEGP(train_x, train_y, likelihood, 3, ODE_name="Bipendulum")
-    
-    x = var('x')
-    u1 = function('u1')(x)
-    u2 = function('u2')(x)
-    u3 = function('u3')(x)
-    
-    functions = [u1, u2, u3]  
-    differential_eq = list(model.A.rows())  
-    sage_locals = model.sage_locals
+    ode_row_to_check = 1
+
+    # Verify that the models output satisfies the given differential equation
+    target_row = 2
+    # Row is to be used with VkV' * A
+    # Apply to second element of k
+    model_cov_fkt_row = model.return_cov_fkt_row(target_row)
+    diff_var = var("t2")
+    differential_equation = [term.substitute(x=diff_var) for term in sage_eval(str(model.A[ode_row_to_check]), locals=model.sage_locals)]
+    print(calculate_differential_equation_error_symbolic(model_cov_fkt_row, differential_equation, model.sage_locals, diff_var=diff_var)(t1=1, t2=1, signal_variance_2=1.0, lengthscale_2=1.0, a=1.0, b=1.0))
 
 
-    result = calculate_differential_equation_error_symbolic(functions, differential_eq, sage_locals)
-    print("Symbolischer ODE-Fehler:", result)
-
-
+    # Column is to be used with A * VkV'
+    # Apply to first element of k
+    target_col = 0
+    model_cov_fkt_col = model.return_cov_fkt_col(target_col)
+    diff_var = var("t1")
+    differential_equation = [term.substitute(x=diff_var) for term in sage_eval(str(model.A[ode_row_to_check]), locals=model.sage_locals)]
+    print(calculate_differential_equation_error_symbolic(model_cov_fkt_col, differential_equation, model.sage_locals, diff_var=diff_var)(t1=1, t2=1, signal_variance_2=1.0, lengthscale_2=1.0, a=1.0, b=1.0))
 
 
 
 
 def test_calculate_differential_equation_error_numeric():
+
+
+    # Trainingsdaten basierend auf e^x
     train_x = torch.linspace(0, 1, 10)
-    train_y = torch.linspace(0, 1, 10)
-    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(3, noise_constraint=gpytorch.constraints.Positive())
-    model = LODEGP(train_x, train_y, likelihood, 3, ODE_name="Bipendulum")
-    differential_eq = list(model.A.rows())  
+    train_y = torch.exp(train_x)  # e^x
+    # packen wir in 2D (z.B. y und y')
+    train_y = torch.stack([train_y, train_y], -1)  
+
+    # Likelihood und Modell
+    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(
+        2, noise_constraint=gpytorch.constraints.Positive()
+    )
+    model = LODEGP(train_x, train_y, likelihood, 2, ODE_name="Minimal")
+
+    # Lokale Werte
+    local_values = model.prepare_numeric_ode_satisfaction_check()
+    print("Lokale Werte:", local_values)
+
+    # Differentialgleichung vorbereiten
+    target_row = 1
+    ode_row_to_check = 0
+    diff_var = var("t2")
+    differential_eq = [
+        term.substitute(x=diff_var)
+        for term in sage_eval(str(model.A[ode_row_to_check]), locals=model.sage_locals)
+    ]
+
+    model.eval()
+    likelihood.eval()
     sage_locals = model.sage_locals
-    data = np.linspace(0, 2*np.pi, 100)
-    data_generating_functions = [lambda x: np.sin(x), lambda x: np.cos(x)]
-    result = calculate_differential_equation_error_numeric(differential_eq, sage_locals, data_generating_functions, data)
+    data = torch.linspace(0, 3, 200)  # Testbereich etwas größer
+
+    # Daten generierende Funktion: Modell-Ausgabe
+    data_generating_functions = lambda x: model(x).mean
+
+    # Numerischen ODE-Fehler berechnen
+    with gpytorch.settings.prior_mode(True):
+        result = calculate_differential_equation_error_numeric(
+            differential_eq,
+            sage_locals,
+            data_generating_functions,        
+            data,
+            locals_values=local_values
+        )
+
     print("Numerischer ODE-Fehler:", result)
+    plt.plot(data, result.detach(), label="ODE Fehler")
+    plt.axhline(0, color="black", linestyle="--")
+    plt.legend()
+    plt.show()
+
   
 
 
